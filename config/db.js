7d853ea18925d5dff9cbf2c543b198c73e09190c
Johann Folta <jfolta@kyruus.com>
2020-04-27 12:34:05 -0400

    update mongoose.connect() with async/await and try/catch

--- config/db.js
+++ config/db.js
@@ -2,19 +2,36 @@ const mongoose = require("mongoose");
 const config = require("config");
 const db = config.get("mongoURI"); // gets the URI (with database username and pwd) from config/default.json's mongoURI key
 
-const connectDB = () => {
-  mongoose
-    .connect(db, {
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
       useNewUrlParser: true,
       useCreateIndex: true,
       useFindAndModify: false,
       useUnifiedTopology: true,
-    })
-    .then(() => console.log("MongoDB connected..."))
-    .catch((err) => {
-      console.err(err.message);
-      process.exit(1);
     });
+    console.log("MongoDB connected...");
+  } catch (err) {
+    console.err(err.message);
+    process.exit(1);
+  }
 };
+
+// REFACTORED above with ASYNC/AWAIT & TRY/CATCH
+
+// const connectDB = () => {
+//   mongoose
+//     .connect(db, {
+//       useNewUrlParser: true,
+//       useCreateIndex: true,
+//       useFindAndModify: false,
+//       useUnifiedTopology: true,
+//     })
+//     .then(() => console.log("MongoDB connected..."))
+//     .catch((err) => {
+//       console.err(err.message);
+//       process.exit(1);
+//     });
+// };
 
 module.exports = connectDB;
