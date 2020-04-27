59902e9a3dd5f2b0b6fb76b5f7254b57ba8addc1
Johann Folta <jfolta@kyruus.com>
2020-04-27 12:11:15 -0400

    routes setup (testable on Postman): users, auth, contacts (CRUD)

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-// Using CommonJS's require (can't import like in React)
+// Using CommonJS's require (can't ES6 import like in React)
 const express = require("express");
 
 const app = express();
@@ -6,6 +6,11 @@ const app = express();
 app.get("/", (req, res) =>
   res.json({ msg: "Welcome to the ContactKeeper API" })
 );
+
+// Define Routes
+app.use("/api/users", require("./routes/users"));
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/contacts", require("./routes/contacts"));
 
 const PORT = process.env.PORT || 5000; // process.env.PORT for PROD
 
