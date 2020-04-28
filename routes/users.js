e516391e2d96dfb897a19c2293af6e845c7e0c2e
Johann Folta <jfolta@kyruus.com>
2020-04-28 08:27:14 -0400

    jwt jsonwebtoken setup

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
 const { check, validationResult } = require("express-validator");
 // https://express-validator.github.io/docs/index.html
 
@@ -53,7 +55,23 @@ router.post(
 
       await user.save();
 
-      res.send("User saved");
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"), // gets the jwtSecret from config/default.json's jwtSecret key
+        {
+          expiresIn: 360000,
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
     } catch (err) {
       console.error(err.message);
       res.status(500).send("Server Error");
