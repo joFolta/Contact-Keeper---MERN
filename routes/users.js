1755861be2fce531adf0e1b07bc5b9765d01789e
Johann Folta <jfolta@kyruus.com>
2020-04-28 08:12:39 -0400

    save user and hash passsword with bcryptjs

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
 const { check, validationResult } = require("express-validator");
 // https://express-validator.github.io/docs/index.html
 
@@ -25,13 +26,38 @@ router.post(
       .matches(/\d/)
       .withMessage("Must contain a number"),
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
 
-    res.send("passed");
+    const { name, email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email: email }); // ES6 shorthand: findOne({ email })
+
+      if (user) {
+        return res.status(400).json({ msg: "User already exists" });
+      }
+
+      user = new User({
+        name: name, // ES6 shorthand: name
+        email: email, // ES6 shorthand: email
+        password: password, // ES6 shorthand: password
+      });
+
+      const salt = await bcrypt.genSalt(10); // bcrypt password encryption/hashing
+
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+
+      res.send("User saved");
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server Error");
+    }
   }
 );
 
