4cd3f0852676a6273395b3f8352fa776ab7a9bfd
Johann Folta <jfolta@kyruus.com>
2020-05-05 14:23:50 -0400

    contact model and get contact route

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,12 +1,29 @@
 const express = require("express");
 const router = express.Router();
+const auth = require("../middleware/auth");
+const { check, validationResult } = require("express-validator");
+
+const User = require("../models/User");
+const Contact = require("../models/Contact");
 
 // @route   GET api/contacts
 // @desc    Get all of a user's contacts
 // @access  Private
-router.get("/", (req, res) => {
-  res.send("Get all contacts");
-});
+router.get(
+  "/",
+  auth, // runs middleware/auth (send header 'x-auth-token'; pulls out payload and assigns to req.user)
+  async (req, res) => {
+    try {
+      const contacts = await Contact.find({ user: req.user.id }).sort({
+        date: -1, // sort by newest contact
+      });
+      res.json(contacts);
+    } catch (error) {
+      console.error(err.message);
+      res.status(500).send("Server Error");
+    }
+  }
+);
 
 // @route   POST api/contacts
 // @desc    Add new contacts
